d6fc83d6410a828f2633f5fb8dccd8decec4ba74
Your Name <BenArmour72@gmail.com>
2023-09-17 02:56:40 -0400

    Spawning

--- Assets/Scripts/Arena1Game.cs
+++ Assets/Scripts/Arena1Game.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Netcode;
 using UnityEngine;
+using Unity.Netcode;
 
 public class Arena1Game : NetworkBehaviour
 {
+    public Player playerPrefab;
+    public Camera arenaCamera;
+
     private int positionIndex = 0;
     private Vector3[] startPositions = new Vector3[]
     {
@@ -14,49 +17,26 @@ public class Arena1Game : NetworkBehaviour
         new Vector3(0, 2, -4)
     };
 
-    private int colorIndex = 0;
-    private Color[] playerColors = new Color[] {
-        Color.blue,
-        Color.green,
-        Color.yellow,
-        Color.magenta,
-    };
-
-    public GameObject playerPrefab; 
-
-    private Vector3 NextPosition()
+    void Start()
     {
+            SpawnPlayers();
+    }
+
+    private Vector3 NextPosition() {
         Vector3 pos = startPositions[positionIndex];
         positionIndex += 1;
-        if (positionIndex > startPositions.Length - 1)
-        {
+        if (positionIndex > startPositions.Length - 1) {
             positionIndex = 0;
         }
         return pos;
     }
 
-    private Color NextColor()
+    private void SpawnPlayers()
     {
-        Color newColor = playerColors[colorIndex];
-        colorIndex += 1;
-        if (colorIndex > playerColors.Length - 1)
+        foreach (ulong clientId in NetworkManager.ConnectedClientsIds)
         {
-            colorIndex = 0;
+            Player playerSpawn = Instantiate(playerPrefab, NextPosition(), Quaternion.identity);
+            playerSpawn.GetComponent<NetworkObject>().SpawnWithOwnership(clientId);
         }
-        return newColor;
-    }
-
-    [ServerRpc]
-    public void RequestSpawnPlayerServerRpc(ulong clientId)
-    {
-        Vector3 spawnPosition = NextPosition();
-        Color assignedColor = NextColor();
-
-        GameObject playerInstance = Instantiate(playerPrefab, spawnPosition, Quaternion.identity);
-        PlayerController playerController = playerInstance.GetComponent<PlayerController>();
-
-        playerController.playerColor.Value = assignedColor;
-
-        playerInstance.GetComponent<NetworkObject>().SpawnWithOwnership(clientId);
     }
 }
