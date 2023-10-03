190b57d8ded6b8955eb90fdcb339d98052d04254
Your Name <BenArmour72@gmail.com>
2023-10-02 23:54:53 -0400

    more refactor of player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,22 +15,23 @@ public class Player : NetworkBehaviour
     [Header("Aiming Settings")]
     [SerializeField] private float aimingFOV = 40f;
     private const float FOV_LERP_SPEED = 5f;
-    private float normalFOV;
+    private const float MOUSE_Y_MULTIPLIER = -1f;
+    private const float EXTRA_HEIGHT_TEST = 0.1f;
 
+    private float normalFOV;
     private Camera playerCamera;
     private Vector3 moveDirection = Vector3.zero;
     private CharacterController characterController;
     private float verticalLookRotation = 0f;
     private bool isReloading = false;
     private PlayerAnimationHandler playerAnimationHandler;
-    
     private NetworkVariable<Vector3> networkedPosition = new NetworkVariable<Vector3>();
     private NetworkVariable<Quaternion> networkedRotation = new NetworkVariable<Quaternion>();
 
     private void Awake()
     {
         InitializeComponents();
-        normalFOV = playerCamera.fieldOfView; 
+        normalFOV = playerCamera.fieldOfView;
     }
 
     private void InitializeComponents()
@@ -71,18 +72,19 @@ public class Player : NetworkBehaviour
     {
         if (IsOwner)
         {
-            HandlePlayerInputAndMovement();
+            OwnerHandleInput();
             HandleMouseLook();
         }
     }
 
-    private bool IsPlayerGrounded()
+    private void OwnerHandleInput()
     {
-        float extraHeightTest = 0.1f;
-        return Physics.SphereCast(characterController.bounds.center, characterController.radius, Vector3.down, out RaycastHit hit, characterController.bounds.extents.y + extraHeightTest);
+        HandleMovementInput();
+        HandleMouseInput();
+        HandleActionInput();
     }
 
-    private void HandlePlayerInputAndMovement()
+    private void HandleMovementInput()
     {
         float xMove = Input.GetAxis("Horizontal");
         float zMove = Input.GetAxis("Vertical");
@@ -96,34 +98,60 @@ public class Player : NetworkBehaviour
         moveDirection.y -= gravity * Time.deltaTime;
 
         playerAnimationHandler.UpdateMovementAnimations(xMove, zMove);
+    }
 
+    private void HandleMouseInput()
+    {
         if (Input.GetMouseButton(1))
         {
             playerAnimationHandler.SetAiming(true);
-            playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, aimingFOV, Time.deltaTime * 5f);
+            playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, aimingFOV, Time.deltaTime * FOV_LERP_SPEED);
         }
         else
         {
             playerAnimationHandler.SetAiming(false);
-            playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, normalFOV, Time.deltaTime * 5f);
+            playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, normalFOV, Time.deltaTime * FOV_LERP_SPEED);
         }
+    }
 
+    private void HandleActionInput()
+    {
+        HandleShootingInput();
+        HandleReloadingInput();
+        HandleRollInput();
+        HandleJumpInput();
+        HandlePickupInput();
+
+        MoveServerRpc(moveDirection * Time.deltaTime);
+    }
+
+    private void HandleShootingInput()
+    {
         if (Input.GetMouseButtonDown(0) && !isReloading)
         {
             playerAnimationHandler.TriggerShootAnimation();
         }
+    }
 
+    private void HandleReloadingInput()
+    {
         if (Input.GetKeyDown(KeyCode.R) && !isReloading)
         {
             isReloading = true;
             playerAnimationHandler.TriggerReloadAnimation();
         }
+    }
 
+    private void HandleRollInput()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             playerAnimationHandler.TriggerRollAnimation();
         }
+    }
 
+    private void HandleJumpInput()
+    {
         if (IsPlayerGrounded() && Input.GetButtonDown("Jump"))
         {
             moveDirection.y = jumpForce;
@@ -133,13 +161,14 @@ public class Player : NetworkBehaviour
         {
             playerAnimationHandler.SetJumping(0.0f);
         }
+    }
 
+    private void HandlePickupInput()
+    {
         if (Input.GetKeyDown(KeyCode.E))
         {
             playerAnimationHandler.TriggerPickupAnimation();
         }
-
-        MoveServerRpc(moveDirection * Time.deltaTime);
     }
 
     private void HandleMouseLook()
@@ -147,17 +176,21 @@ public class Player : NetworkBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-        verticalLookRotation -= mouseY;
+        verticalLookRotation += mouseY * MOUSE_Y_MULTIPLIER;
         verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
 
         playerCamera.transform.localRotation = Quaternion.Euler(verticalLookRotation, 0f, 0f);
+        RotatePlayer(mouseX);
+    }
 
+    private void RotatePlayer(float mouseX)
+    {
         Vector3 rotation = Vector3.up * mouseX;
         transform.Rotate(rotation);
         RotateServerRpc(rotation);
     }
 
-      private void OnPositionChanged(Vector3 oldValue, Vector3 newValue)
+    private void OnPositionChanged(Vector3 oldValue, Vector3 newValue)
     {
         transform.position = newValue;
     }
@@ -196,7 +229,12 @@ public class Player : NetworkBehaviour
         }
     }
 
-   private bool IsHostPlayer()
+    private bool IsPlayerGrounded()
+    {
+        return Physics.SphereCast(characterController.bounds.center, characterController.radius, Vector3.down, out RaycastHit hit, characterController.bounds.extents.y + EXTRA_HEIGHT_TEST);
+    }
+
+    private bool IsHostPlayer()
     {
         return NetworkManager.Singleton != null && NetworkManager.Singleton.LocalClientId == NetworkManager.ServerClientId;
     }
